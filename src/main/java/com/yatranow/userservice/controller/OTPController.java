d1ef07f56bb48908fa2945d1d0811eadde0934f1
javasoftarun <javasoftarun159@gmail.com>
2025-04-28 21:02:43 +0530

    Added cross origin allowed

--- src/main/java/com/yatranow/userservice/controller/OTPController.java
+++ src/main/java/com/yatranow/userservice/controller/OTPController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ import com.yatranow.userservice.service.OtpService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users/otp")
 public class OTPController {
--- src/main/java/com/yatranow/userservice/controller/PasswordResetController.java
+++ src/main/java/com/yatranow/userservice/controller/PasswordResetController.java
@@ -2,6 +2,7 @@ package com.yatranow.userservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.yatranow.userservice.service.PasswordResetService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users")
 public class PasswordResetController {
