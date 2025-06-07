4457a9502ba2aca0456aee6e9575b071ea513dc1
javasoftarun <javasoftarun159@gmail.com>
2025-06-07 16:09:45 +0530

    firebase

--- src/main/java/com/yatranow/userservice/config/FirebaseConfig.java
+++ src/main/java/com/yatranow/userservice/config/FirebaseConfig.java
@@ -5,8 +5,10 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Base64;
 
 import javax.annotation.PostConstruct;
 
@@ -15,14 +17,17 @@ public class FirebaseConfig {
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            }
+        	String base64Key = System.getenv("FIREBASE_SERVICE_ACCOUNT");
+        	if (base64Key == null) {
+        	    throw new IllegalStateException("FIREBASE_SERVICE_ACCOUNT env variable not set");
+        	}
+        	byte[] decodedKey = Base64.getDecoder().decode(base64Key);
+        	FirebaseOptions options = new FirebaseOptions.Builder()
+        	        .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(decodedKey)))
+        	        .build();
+        	if (FirebaseApp.getApps().isEmpty()) {
+        	    FirebaseApp.initializeApp(options);
+        	}
         } catch (IOException e) {
             e.printStackTrace();
         }
